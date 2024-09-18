23e15d153a0b804871e44668d8562fe04c11cabe
Fathur <fathur260605@gmail.com>
2024-09-18 15:43:59 +0700

    tutorial 3 selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k!iggt52dy%5by!5ube0^ec=ewk@aluz5cbmck+gkxyow)&kr&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fathurrahman-kesuma-mentalhealthtracker.pbp.cs.ui.ac.id", "http://fathurrahman-kesuma-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
